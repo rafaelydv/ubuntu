c59061bafa1d076cde3ee836f3215cd85316b136
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-06-27 19:41:48 -0400

    Incluídos pacotes da instalação customizada do Xfce.

---
+++ 02-xfce-base.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+apt install xorg \
+	    xfwm4 \
+	    xfce4-panel \
+	    xfce4-session \
+	    xfce4-settings \
+	    xfce4-notifyd \
+	    xfce4-date-time-plugin \
+	    xfce4-genmon-plugin \
+	    xfce4-whiskermenu-plugin \
+	    xfce4-taskmanager \
+	    xfce4-screenshooter\
+	    xfce4-pulseaudio-plugin \
+	    xfce4-power-manager \
+	    xfce4-notes \
+	    terminator \
+	    xdg-utils \
+	    pcmanfm \
+	    gvfs-fuse \
+	    gvfs-backends \
+	    lightdm-gtk-greeter-settings
+	    pavucontrol \
+            p7zip-full \
+	    xarchiver \
+	    rar \
+	    unrar
