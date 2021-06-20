7e71f481472ea5bb54b334ae3b642961fbb52051
rafaelydv <56797088+rafaelydv@users.noreply.github.com>
2021-06-20 17:53:40 -0400

    Delete gnome_install directory

--- gnome_install/03-temas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-apt install arc-theme \
-	    numix-gtk-theme \
-	    deepin-icon-theme \
-	    breeze-cursor-theme \
-	    moblin-cursor-theme \
-	    dmz-cursor-theme
--- gnome_install/04-internet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-apt install firefox \
-	    firefox-locale-pt \
-	    transmission \
-	    transmission-gtk \
-	    filezilla
